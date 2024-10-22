73268319756c023e215f6ebcecfa9f7fb233a2b0
Pegasus-Prongs <108112143+Pegasus-Prongs@users.noreply.github.com>
2024-10-22 22:06:28 +0700

    add static section, call pad, history, task.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import { Grid, Badge, Button, IconButton, InputAdornment, TextField, Typography, Container } from '@mui/material';
+import { Grid, Badge, Button, IconButton, InputAdornment, TextField, Typography, Container, Chip } from '@mui/material';
+import { Paper, Box } from '@mui/material';
 import { NotificationAddOutlined, SearchOutlined, LogoutOutlined } from '@mui/icons-material';
+import { AccessTime, Call, PhoneInTalk, MonetizationOn, CheckCircle, Assessment } from '@mui/icons-material';
+import MultipleSelectChip from './components/MultiChip';
+import EditorWithBottomToolbar from './components/EditorWithBottomToolbar';
+import CallPad from './components/CallPad';
 
 function App() {
   return (
@@ -11,7 +16,7 @@ function App() {
         </Grid>
 
         {/* Top Bar */}
-        <Grid container item xs={12} spacing={2} sx={{ py: 2 }}>
+        <Grid container item xs={12} spacing={2} sx={{ py: 2, px: 2 }}>
           <Grid item xs={2}>
             <Button variant="contained">Call</Button>
           </Grid>
@@ -49,22 +54,98 @@ function App() {
         </Grid>
 
         {/* Content Area */}
-        <Grid container columns={16} spacing={2}>
+        <Grid container columns={15} spacing={2}>
           {/* Vertical Button List */}
           <Grid container item xs={2}>
-            <div className="flex flex-col gap-2">
-              <Button variant="contained">SMS</Button>
-              <Button variant="contained">Email</Button>
-              <Button variant="contained">My Callback</Button>
-              <Button variant="contained">Notes</Button>
-              <Button variant="contained">Task</Button>
-              <Button variant="contained">View agents online</Button>
-              <Button variant="contained">Chat</Button>
+            <div className="flex flex-col gap-4">
+              {/* Action Buttons */}
+              <div className="flex flex-col gap-2">
+                <Button variant="contained" fullWidth>SMS</Button>
+                <Button variant="contained" fullWidth>Email</Button>
+                <Button variant="contained" fullWidth>My Callback</Button>
+                <Button variant="contained" fullWidth>Notes</Button>
+                <Button variant="contained" fullWidth>Task</Button>
+                <Button variant="contained" fullWidth>View Agents Online</Button>
+                <Button variant="contained" fullWidth>Chat</Button>
+              </div>
+
+              {/* Statistics Section */}
+              <div className="flex flex-col gap-4 p-4 bg-gray-100 rounded-lg shadow-md">
+                <Typography variant="h6" className="font-bold text-gray-800 mb-2">Agent Daily stats</Typography>
+                <Grid container spacing={2} direction="row">
+                  <Grid item xs={12}>
+                    <Paper elevation={3} className="p-4 flex items-center rounded-md">
+                      <Box className="mr-2">
+                        <Call color="primary" />
+                      </Box>
+                      <div className="flex-1">
+                        <Typography variant="body1" className="font-semibold text-gray-800">Total Calls</Typography>
+                        <Typography variant="body1" className="text-gray-600">120</Typography>
+                      </div>
+                    </Paper>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Paper elevation={3} className="p-4 flex items-center rounded-md">
+                      <Box className="mr-2">
+                        <PhoneInTalk color="primary" />
+                      </Box>
+                      <div className="flex-1">
+                        <Typography variant="body1" className="font-semibold text-gray-800">Outbound Calls</Typography>
+                        <Typography variant="body1" className="text-gray-600">80</Typography>
+                      </div>
+                    </Paper>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Paper elevation={3} className="p-4 flex items-center rounded-md">
+                      <Box className="mr-2">
+                        <MonetizationOn color="primary" />
+                      </Box>
+                      <div className="flex-1">
+                        <Typography variant="body1" className="font-semibold text-gray-800">No. of Sales</Typography>
+                        <Typography variant="body1" className="text-gray-600">25</Typography>
+                      </div>
+                    </Paper>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Paper elevation={3} className="p-4 flex items-center rounded-md">
+                      <Box className="mr-2">
+                        <CheckCircle color="primary" />
+                      </Box>
+                      <div className="flex-1">
+                        <Typography variant="body1" className="font-semibold text-gray-800">Closed Rates</Typography>
+                        <Typography variant="body1" className="text-gray-600">65%</Typography>
+                      </div>
+                    </Paper>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Paper elevation={3} className="p-4 flex items-center rounded-md">
+                      <Box className="mr-2">
+                        <Assessment color="primary" />
+                      </Box>
+                      <div className="flex-1">
+                        <Typography variant="body1" className="font-semibold text-gray-800">Avg. Talk Time</Typography>
+                        <Typography variant="body1" className="text-gray-600">5 mins</Typography>
+                      </div>
+                    </Paper>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Paper elevation={3} className="p-4 flex items-center rounded-md">
+                      <Box className="mr-2">
+                        <AccessTime color="primary" />
+                      </Box>
+                      <div className="flex-1">
+                        <Typography variant="body1" className="font-semibold text-gray-800">Inbound Calls</Typography>
+                        <Typography variant="body1" className="text-gray-600">40</Typography>
+                      </div>
+                    </Paper>
+                  </Grid>
+                </Grid>
+              </div>
             </div>
           </Grid>
 
           {/* Information & Activity Section */}
-          <Grid item xs={5} sx={{ mt: 2, mb: 2, gap: 2 }}>
+          <Grid item xs={4} gap={4} sx={{ mt: 2, mb: 2, }}>
             {/* Information Box */}
             <div className="flex flex-col gap-2 p-6 bg-white shadow-lg rounded-lg max-w-lg">
               <div className="flex flex-row justify-between border-b">
@@ -148,18 +229,107 @@ function App() {
                 </Button>
               </div>
             </div>
+            <Paper
+              elevation={3}
+              style={{
+                height: '300px',
+                borderRadius: '12px', // Rounded corners
+                padding: '16px',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+                boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)', // Shadow effect
+              }}
+            >
+              <Typography variant="h6" className="font-bold" gutterBottom>
+                Tasks
+              </Typography>
+              <div className="flex-1 overflow-y-auto">
+                <Typography variant="body1">1. Follow up with clients</Typography>
+                <Typography variant="body1">2. Update the project status</Typography>
+                <Typography variant="body1">3. Prepare the meeting agenda</Typography>
+                <Typography variant="body1">4. Review team feedback</Typography>
+                <Typography variant="body1">5. Plan next week's tasks</Typography>
+              </div>
+            </Paper>
           </Grid>
-          <Grid item container xs={8} columns={12}>
-              <Grid item container xs={12}>
-                <Grid item xs={4}>
-                  14.Script
-                </Grid>
-                <Grid item xs={8}
-                ></Grid>
+          <Grid item xs container gap={4} columns={12} sx={{
+            padding: 2
+          }}>
+            <Grid item container xs={12}>
+              <Grid item xs={4}>
+                14.Script
               </Grid>
-              <Grid item xs={12}>
-                
+              <Grid item xs={8}>
+                <CallPad></CallPad>
               </Grid>
+            </Grid>
+            <Grid container item xs={12} className='rounded-lg shadow-lg p-4 border-2'>
+              <Typography variant='h6'>SMS TYPING</Typography>
+              <Grid container alignItems="center" sx={{
+                mb: 2
+              }} >
+                <Typography variant='body2' component="span">To:</Typography>
+                <TextField label="SMS Number" size='small' variant="outlined" />
+                <Button variant='contained'>SEND</Button>
+              </Grid>
+              <TextField
+                label="Message"
+                variant='outlined'
+                multiline
+                rows={4}
+                maxRows={4} fullWidth />
+            </Grid>
+            <Grid item container xs={12}>
+              <Grid xs={12}>
+                <div className='flex flex-col bg-white shadow-xl rounded-md w-full p-4'>
+                  <div className="space-y-4">
+                    <div className="flex items-center gap-4">
+                      <Typography component="span" variant="h6" className="w-1/5">From</Typography>
+                      <Typography
+                        variant="outlined"
+                        className="w-4/5"
+                        placeholder="Enter sender's email"
+                        size="small"
+                      >
+                        jonedoe@test.com
+                      </Typography>
+                    </div>
+
+                    {/* To */}
+                    <div className="flex items-center gap-4">
+                      <Typography component="span" variant="h6" className="w-1/5">To</Typography>
+                      <MultipleSelectChip className="w-4/5" />
+                    </div>
+
+                    {/* Subject */}
+                    <div className="flex items-center gap-4">
+                      <Typography component="span" variant="h6" className="w-1/5">Subject</Typography>
+                      <TextField
+                        variant="outlined"
+                        className="w-4/5"
+                        placeholder="Enter subject"
+                        size="small"
+                      />
+                    </div>
+
+                    {/* Content */}
+                    <div>
+                      <EditorWithBottomToolbar />
+                    </div>
+
+                    {/* Send Button */}
+                    <div className="flex justify-end">
+                      <Button variant="contained" color="primary">
+                        Send
+                      </Button>
+                    </div>
+                  </div>
+
+                </div>
+              </Grid>
+            </Grid>
+
           </Grid>
         </Grid>
       </Grid>
