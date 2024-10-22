707631bd95f8adc3e70fa78c465dcac277022f1c
Pegasus-Prongs <pegasusprongs@gmail.com>
2024-10-22 10:19:00 +0700

    update package and add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Correct pattern to scan files in `src` and subdirectories
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
